4cfb1ef6e7513f14029397618c8bf98f6ff8f433
GabrielaChoquepata <galabriela.art@gmail.com>
2022-02-15 11:19:58 -0500

    Avance 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+window.addEventListener("load",inicio,true);
+function inicio(){
+    document.getElementById("text-mssg").addEventListener("keyup",function(){
+        this.value=this.value.toUpperCase();
+    })
+}document.getElementById("encode-button").addEventListener("")
