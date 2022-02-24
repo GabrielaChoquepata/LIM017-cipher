d9a777290aa57cb2a65400936163cd542b89b851
GabrielaChoquepata <galabriela.art@gmail.com>
2022-02-24 10:30:41 -0500

    Avance 2

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,46 @@
+// function trace (str) {
+//   console.log(Date(Date.now()),str);
+// }
+// export default trace;
 const cipher = {
-  // ...
+  encode: (offset = undefined, string = "") => {
+      if (typeof offset !== "number") throw new TypeError("El valor ingresado no es un numero");
+      if (!string) throw new TypeError('No ingresaste ningun mensaje');
+      if (typeof string !== "string") throw new TypeError("El valor ingresado no es texto");
+
+      let messgCode = ""
+      for (let i = 0; i < string.length; i++) {
+          if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+              let posAssci = string.charCodeAt(i);
+              let posOffset = (posAssci - 65 + offset) % 26 + 65;
+              let letterResult = String.fromCharCode(posOffset);
+              messgCode = messgCode + letterResult;
+          } else {
+            messgCode += string[i];
+          }
+
+      }
+      return messgCode
+  },
+  decode: (offset = undefined, string = "") => {
+      if (!string) throw new TypeError('No ingresaste ningun mensaje');
+      if (typeof string !== "string") throw new TypeError("El valor ingresado no es texto");
+      if (typeof offset !== "number") throw new TypeError("El valor ingresado no es un numero");
+
+      let messgDecode = ""
+      for (let i = 0; i < string.length; i++) {
+          if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+              let posAssci = string.charCodeAt(i);
+              let posOffset = (posAssci + 65 - offset) % 26 + 65;
+              let letterResult = String.fromCharCode(posOffset);
+              messgDecode = messgDecode + letterResult;
+          } else {
+            messgDecode += string[i];
+          }
+
+      }
+      return messgDecode
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,63 @@
-import cipher from './cipher.js';
+// import trace from "./cipher.js";
+// trace("hola a todos")
 
-console.log(cipher);
+import cipher from "./cipher.js";
+
+const mostrarW2 = document.getElementById("btnmostrarW2");
+const mostrarCipher = document.getElementById("btnmostrarCipher");
+const encodeButton = document.getElementById("encode-button");
+const decodeButton = document.getElementById("decode-button");
+const clearBtn = document.getElementById("clear-button");
 
 
-window.addEventListener("load",inicio,true);
-function inicio(){
-    document.getElementById("text-mssg").addEventListener("keyup",function(){
-        this.value=this.value.toUpperCase();
-    })
-}document.getElementById("encode-button").addEventListener("")
+mostrarW2.addEventListener("click", () => {
+  let username = document.getElementById("username").value;
+  if (username === '') {
+      username = 'Desconocidx';
+  }
+  document.getElementById("W1").style.display = "none";
+  document.getElementById("W2").style.display = "block";document.getElementById("personalised message").innerHTML = ('HEY! Hola! ' + username + '<br>Bienvenidx a Gravity falls! <br><br> Un misterioso pueblo lleno de secretos. <br>Para descubrir las pistas y misiones que te daremos utilizaremos mensajes cifrados, para ello inventamos un codigo secreto llamado "cipher". Este funciona cambiando el orden de las letras del alfabeto, desplazando las letras en un orden especifico, que es un número clave.');});
+
+
+mostrarCipher.addEventListener("click", () => {
+  document.getElementById("W2").style.display = 'none';
+  document.getElementById("W3").style.display = 'block';
+});
+
+
+encodeButton.addEventListener("click", e => {
+    e.preventDefault();
+    const input = document.getElementById("text-meng");
+    const value = input.value;
+    const valueUper = value.toUpperCase();
+    const offSet = parseInt(document.getElementById("offset").value);
+
+    const valueEncode = cipher.encode(offSet, valueUper);
+
+    const finalResult = document.getElementById("result");
+    finalResult.innerHTML = valueEncode;
+
+});
+
+
+decodeButton.addEventListener("click", e => {
+    e.preventDefault();
+    const input = document.getElementById("text-meng");
+    const value = input.value;
+    const valueUper = value.toUpperCase();
+    const offSet = parseInt(document.getElementById("offset").value);
+
+    const valueDecode = cipher.decode(offSet, valueUper);
+
+    const finalResult = document.getElementById("result");
+    finalResult.innerHTML = valueDecode;
+
+});
+
+clearBtn.addEventListener("click", e => {
+    e.preventDefault();
+    document.getElementById("text-meng").value = "";
+    document.getElementById("offset").value = "";
+    const finalResult = document.getElementById("result");
+    finalResult.innerHTML = "";
+});
